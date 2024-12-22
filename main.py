53fa71a41090cd86b66494f0592adbff2fb6b178
shahdkh2288 <101909725+shahdkh2288@users.noreply.github.com>
2024-12-22 05:49:57 +0200

    added neural networks models

--- main.py
+++ main.py
@@ -2,18 +2,24 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
-
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import classification_report, confusion_matrix, f1_score
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.utils import to_categorical
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.utils import shuffle
+from tensorflow.keras.layers import Input
+from tensorflow import keras
 
 
 def load_data(csv_path_: str):
     # Load dataset
     data = pd.read_csv(csv_path_)
-    # Preview data
     print(data.head())
 
     return data
+
 
 def split_data(data: pd.DataFrame):
     # split data to features and label
@@ -22,6 +28,12 @@ def split_data(data: pd.DataFrame):
 
     return X, y
 
+
+def preprocess_labels(y: pd.Series):
+    y_encodingHot = to_categorical(y, num_classes=26)  # One-hot encode
+    return y_encodingHot
+
+
 def show_images(X: pd.DataFrame, y: pd.Series, n_images: int):
     # Display sample images
 
@@ -29,7 +41,7 @@ def show_images(X: pd.DataFrame, y: pd.Series, n_images: int):
     X_images = X.values.reshape(-1, 28, 28)
 
     # Plot the first 10 images
-    plt.figure(figsize=(5, 5))
+    plt.figure(figsize=(10, 5))
     for i in range(n_images):
         plt.subplot(2, 5, i + 1)
         plt.imshow(X_images[i], cmap="gray")
@@ -39,16 +51,17 @@ def show_images(X: pd.DataFrame, y: pd.Series, n_images: int):
     plt.tight_layout()
     plt.show()
 
+
 def show_classes_info(y: pd.Series):
-    # show number of unique classes
+    # Show number of unique classes
     print(f"\nNumber of unique classes: {y.nunique()}")
 
-    # show class distribution
+    # Show class distribution
     label_counts = y.value_counts()
 
     # Plot the distribution using Seaborn
     plt.figure(figsize=(12, 6))
-    sns.barplot(hue=label_counts.index,legend=False, y=label_counts.values, palette="viridis")
+    sns.barplot(x=label_counts.index, y=label_counts.values, palette="viridis")
     plt.title("Distribution of Classes in `y`", fontsize=16)
     plt.xlabel("Classes (Alphabets)", fontsize=14)
     plt.ylabel("Count", fontsize=14)
@@ -58,35 +71,207 @@ def show_classes_info(y: pd.Series):
     plt.show()
 
 
+# ........................................
+
+# Neural Network
+def split_dataset(X, y, test_size=0.3, val_size=0.5):
+    X_train, X_temp, y_train, y_temp = train_test_split(X, y, test_size=test_size, random_state=42)
+    X_val, X_test, y_val, y_test = train_test_split(X_temp, y_temp, test_size=val_size, random_state=42)
+    return X_train, X_val, X_test, y_train, y_val, y_test
+
+
+# build Model 1
+def build_neuralnetwork_model_1():
+    model = Sequential([
+        Input(shape=(28, 28)),
+        Flatten(),
+        Dense(128, activation='relu'),  # 128 neurons
+        Dense(64, activation='relu'),  # 64 neurons
+        Dense(26, activation='softmax')
+    ])
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
+# Build Model 2
+def build_neuralnetwork_model_2():
+    model = Sequential([
+        Input(shape=(28, 28)),
+        Flatten(),
+        Dense(256, activation='relu'),  # First layer>  256 neurons
+        Dense(128, activation='tanh'),  # second layer>  128 neurons
+        Dense(64, activation='relu'),  # third layer>  64 neurons
+        Dense(32, activation='relu'),  # 4 layer> 32 neurons
+        Dense(26, activation='softmax')
+    ])
+    model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
+    return model
+
+
+def train_and_plotNN(model, X_train, y_train, X_val, y_val, epochs=5, batch_size=32):
+    # Train the model
+    trained_model = model.fit(X_train, y_train, validation_data=(X_val, y_val), epochs=epochs, batch_size=batch_size)
+
+    plt.figure(figsize=(12, 5))
+
+    # accuracy plot
+    plt.subplot(1, 2, 1)
+    plt.plot(trained_model.history['accuracy'], label='Training Accuracy', color='blue')
+    plt.plot(trained_model.history['val_accuracy'], label='Validation Accuracy', color='orange')
+    plt.title('Model Accuracy')
+    plt.xlabel('Epochs')
+    plt.ylabel('Accuracy')
+    plt.legend()
+
+    # loss plot
+    plt.subplot(1, 2, 2)
+    plt.plot(trained_model.history['loss'], label='Training Loss', color='blue')
+    plt.plot(trained_model.history['val_loss'], label='Validation Loss', color='orange')
+    plt.title('Model Loss')
+    plt.xlabel('Epochs')
+    plt.ylabel('Loss')
+    plt.legend()
+
+    plt.tight_layout()
+    plt.show()
+
+    return trained_model
+
+
+def evaluate_NN(model, x_test, y_test):
+    loss, accuracy = model.evaluate(X_test, y_test)
+    print(f"Test Accuracy: {accuracy:.4f}")
+
+    y_pred = model.predict(x_test)
+    y_pred_classes = y_pred.argmax(axis=1)
+    y_actual_classes = y_test.argmax(axis=1)
+
+    # Confusion Matrix
+    conf_matrix = confusion_matrix(y_actual_classes, y_pred_classes)
+    letter = []
+    for i in range(1, 27):
+        letter.append(chr(96 + i))
+    sns.heatmap(conf_matrix, annot=True, cmap='binary', fmt='.0f', xticklabels=letter, yticklabels=letter)
+    plt.title("Confusion Matrix")
+    plt.xlabel("Predicted Labels")
+    plt.ylabel("Actual Labels")
+    plt.show()
+
+    # F1 Score
+    f1 = f1_score(y_actual_classes, y_pred_classes, average='macro')
+    print(f"\nAverage F1-Score: {f1:.2f}")
+
+
+# ...............................................
+def create_image_for_charFromDataSet(char, data, labels):
+    label = ord(char.upper()) - 65
+    char_indices = np.where(labels == label)[0]
+    index = char_indices[0]
+    image = data.iloc[index].values.reshape(28, 28).astype('float32')
+    image = image / 255.0  # Normalize
+    return image
+
+
+def test_NN_with_name(name, model, data, labels):
+    predictions = []
+    for letter in name:
+        try:
+            image = create_image_for_charFromDataSet(letter, data, labels)
+            final_image = image.reshape(1, 28, 28)
+            pred = model.predict(final_image)
+            pred_letter = np.argmax(pred)
+            predictions.append(pred_letter)
+        except ValueError as e:
+            print(e)
+            predictions.append(None)
+
+    pred_letters = []
+
+
+    for pred in predictions:
+     if pred is not None:
+        pred_letters.append(chr(pred + 65))
+
+     print(f"Predicted letters: {''.join(pred_letters)}")
+
+     plt.figure(figsize=(10, 1))
+    for i, letter in enumerate(name):
+       plt.subplot(1, len(name), i + 1)
+    try:
+        image = create_image_for_charFromDataSet(letter, data, labels)
+        plt.imshow(image, cmap='gray')
+        plt.title(f"Pred: {pred_letters[i]}")
+    except ValueError:
+        plt.title("NA")
+    plt.axis('off')
+plt.show()
+
+# .......................................
+
 if __name__ == "__main__":
-    csv_path = r"C:\Users\abdoe\Desktop\workspace\MachineLearning\A_Z Handwritten Data.csv"
-    train_size = 50000  # Number of training examples
-    test_size = 10000  # Number of testing examples
-
-    # Load the data as a DataFrame from the CSV file
+    csv_path = "../input/az-handwritten-alphabets-in-csv-format/A_Z Handwritten Data.csv"
     data = load_data(csv_path)
-
-    # Split the data into features and labels
     X, y = split_data(data)
 
-    # Display number of unique classes and class distribution
+    # Show Class Distribution and Sample Images
     show_classes_info(y)
 
-    # Display sample images
     show_images(X, y, 10)
 
-    # Normalize pixel values to [0, 1] range by dividing by 255 (max pixel value)
-    X_normalized = X / 255.0
+    # Normalize Data
+    X_normalized = X.to_numpy(dtype="float32") / 255.0
     print(f"\nX_normalized shape: {X_normalized.shape}")
 
-    # # Split the data
-    # X_train = X_normalized[:train_size]
-    # y_train = y[:train_size]
-    # X_test = X_normalized[train_size:train_size + test_size]
-    # y_test = y[train_size:train_size + test_size]
-    #
-    # print(f"\nX_train shape: {X_train.shape}")
-    # print(f"y_train shape: {y_train.shape}")
-    #
-    # print(f"\nX_test shape: {X_test.shape}")
-    # print(f"y_test shape: {y_test.shape}")
+    # .....................................
+
+    y_encoded = preprocess_labels(y)
+
+    X_normalized_reshaped = X_normalized.reshape(-1, 28, 28)
+
+    X_train, X_val, X_test, y_train, y_val, y_test = split_dataset(X_normalized_reshaped, y_encoded)
+
+    # Train and Evaluate Model 1
+    print("\nTraining Model 1:")
+    model1 = build_neuralnetwork_model_1()
+    training1 = train_and_plotNN(model1, X_train, y_train, X_val, y_val)
+    print("\nEvaluating Model 1:")
+    evaluate_NN(model1, X_test, y_test)
+
+    # ...................................
+
+    # Train and Evaluate Model 2
+    print("\nTraining Model 2:")
+    model2 = build_neuralnetwork_model_2()
+    training2 = train_and_plotNN(model2, X_train, y_train, X_val, y_val)
+    print("\nEvaluating Model 2:")
+    evaluate_NN(model2, X_test, y_test)
+    # ...............................................
+
+    # save best model
+    if training2.history['val_accuracy'][-1] > training1.history['val_accuracy'][-1]:
+        model2.save('best_nn_model.h5')
+        print("Model 2 is the best model.")
+        best_model = model2
+    else:
+        model1.save('best_nn_model.h5')
+        print("Model 1 is the best model.")
+        best_model = model1
+    # ...........................................
+    # reload best model
+    best_model = keras.models.load_model('best_nn_model.h5')
+    evaluate_NN(best_model, X_test, y_test)
+    name = "Shahd"
+    name1 = "Aliaa"
+    name2 = "Nahla"
+    test_NN_with_name(name, best_model, X, y)
+    test_NN_with_name(name1, best_model, X, y)
+    test_NN_with_name(name2, best_model, X, y)
+
+
+
+
+
+
+
+
+
